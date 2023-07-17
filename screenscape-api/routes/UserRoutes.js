7e94328b88dce7e5a7d98f0ab6eff49f40ba1f82
Amir <Amirhosseinshirzad@users.noreply.github.com>
2023-07-17 23:23:43 +0200

    get liked movies router added

--- screenscape-api/routes/UserRoutes.js
+++ screenscape-api/routes/UserRoutes.js
@@ -1,5 +1,8 @@
 const { addToLikedMovies } = require("../controllers/UserController");
 const router = require("express").Router();
+
+//routes
 router.post("/add", addToLikedMovies);
+router.get("/liked/:email", getLikedMovies);
 
 module.exports = router;
