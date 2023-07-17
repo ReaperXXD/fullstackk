310ef691f8be6d873cb94eeed532a3c59e207990
Amir <Amirhosseinshirzad@users.noreply.github.com>
2023-07-17 22:41:03 +0200

    mongodb compass/atlas connected

--- screenscape-api/server.js
+++ screenscape-api/server.js
@@ -7,4 +7,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+mongoose
+  .connect("mongodb+srv://amir1234:Amir1234@cluster0.zgov7ln.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected");
+  });
+
 app.listen(5000, console.log("server started"));
