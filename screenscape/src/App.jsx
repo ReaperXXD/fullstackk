4d5db096e5c137f0aa37298aed5ee564f3b7d662
Amir <Amirhosseinshirzad@users.noreply.github.com>
2023-07-17 09:47:35 +0200

    app.jsx updated

--- screenscape/src/App.jsx
+++ screenscape/src/App.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Screenscape from "./pages/Screenscape";
+import Signup from "./pages/Signup";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/" element={<Screenscape />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
