599482d29241562b646cb9f5e3edda3a2096ee7a
Amir <Amirhosseinshirzad@users.noreply.github.com>
2023-07-17 18:18:04 +0200

    header updated

--- screenscape/src/components/Header.jsx
+++ screenscape/src/components/Header.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import logo from "../assets/logo.png";
 
-export default function Header() {
-  return <div>Header</div>;
+export default function Header(props) {
+  const navigate = useNavigate();
+  return (
+    <StyledHeader className="flex a-center j-between">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <button onClick={() => navigate(props.login ? "/login" : "/signup")}>
+        {props.login ? "Log In" : "Sign In"}
+      </button>
+    </StyledHeader>
+  );
 }
+const StyledHeader = styled.header``;
