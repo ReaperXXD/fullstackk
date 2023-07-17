7d1fccc5728917f562f600dc00100497b054559a
Amir <Amirhosseinshirzad@users.noreply.github.com>
2023-07-17 21:27:22 +0200

    final changes to the frontend for now

--- screenscape/src/utils/constants.js
+++ screenscape/src/utils/constants.js
@@ -1,2 +1,2 @@
 export const API_KEY = "782ab87230769fd8e76fcea0f4b709a6";
-export const TMBD_BASE_URL = "https://api.themoviedb.org/3/";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3/";
